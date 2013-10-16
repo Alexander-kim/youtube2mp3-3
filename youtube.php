d6789bd16677cc8b9a48552b581202a1bccd0fcc
Matthias <flashfreezer@gmail.com>
2013-10-16 10:31:12 +0200

    security options

--- youtube.php
+++ youtube.php
@@ -1,8 +1,13 @@
 <?php
-
-$youtubelink = $_GET['youtubelink'];
-echo "Datei wird heruntergeladen...";
-$command = "sh /var/www/youtube/youtube2mp3.sh ".$youtubelink;
-shell_exec($command);
-
+if (isset($_GET['youtubelink'])  && strpos($_GET['youtubelink'],"youtube")!==false)
+{
+	$youtubelink = $_GET['youtubelink'];
+	echo "Datei wird heruntergeladen...";
+	$command = "sh /var/www/youtube/youtube2mp3.sh ".$youtubelink;
+	shell_exec($command);
+}
+else
+{
+	echo "Link fehlt";
+}
 ?>
